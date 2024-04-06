981c3b5357e92881115348ed21711b24297ffae4
agalanis7 <30681428+agalanis7@users.noreply.github.com>
2024-04-06 12:33:29 +0000

    fixed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  get "", to: "main#index"
+
   get  "about", to: "about#index"
 
-  root to: "main#index"
+  get "sign_up", to: "registrations#new"
+
+  post "sign_up", to: "registrations#create"
+
 end
