dece7face7964fbba2ffa1f335b4239e7aec640f
agalanis7 <30681428+agalanis7@users.noreply.github.com>
2024-04-08 10:30:32 +0000

    added logout functionality

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
     def index
-        flash[:notice] = "flash notice"
-        flash[:alert] = "flash alert"
+        if session[:user_id]
+        @user = User.find(session[:user_id])
+        end
     end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,9 @@ class RegistrationsController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to "/", notice: "Success"
+            flash[:notice] = 'Account created'
+            redirect_to "/"
+           
         else
             render :new
     end
