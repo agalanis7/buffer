e72c5a462ee07b7ec5a23ee7854fd56b67b0b0af
agalanis7 <30681428+agalanis7@users.noreply.github.com>
2024-04-08 08:37:43 +0000

    password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+    validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, message: "Must be valid email"
+    validates_confirmation_of :password
 end
